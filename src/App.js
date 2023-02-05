e353aef3801783c0a54bf0a900ffab70602f04fb
Finding-Paradise <janarplanet.kg@gmail.com>
2023-02-05 18:44:31 +0600

    initial setup
    removed default images, added google fonts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
-    <div>Hi, Mom!</div>
+    <div className='App'>
+      <HomePage />
+    </div>
   )
 }
 
