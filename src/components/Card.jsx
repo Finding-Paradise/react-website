b09939cd028ebedb4abb745dc46c723fb5c4629c
Finding-Paradise <janarplanet.kg@gmail.com>
2023-02-09 14:24:15 +0600

    added content to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import '../components/Card.css'
 
-function Card() {
+function Card({num1, num2, text}) {
   return (
     <div className='card'>
-        <h1><span className='grey-h1'>0</span><span className='yellow-h1'>1</span></h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        <h1><span className='grey-h1'>{num1}</span><span className='yellow-h1'>{num2}</span></h1>
+        <p>{text}
         </p>
     </div>
   )
