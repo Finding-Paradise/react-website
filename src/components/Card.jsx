311711365bf95054c6f69e9241f1ae845a4ec5de
Finding-Paradise <janarplanet.kg@gmail.com>
2023-02-10 11:00:59 +0600

    fixing adaptive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,8 +5,7 @@ function Card({num1, num2, text}) {
   return (
     <div className='card'>
         <h1><span className='grey-h1'>{num1}</span><span className='yellow-h1'>{num2}</span></h1>
-        <p>{text}
-        </p>
+        <p>{text}</p>
     </div>
   )
 }
